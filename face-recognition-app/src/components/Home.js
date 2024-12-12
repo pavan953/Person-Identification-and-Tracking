23888e2938b620996eb645a716aa19dd5a0e488a
pavan953 <pk5684865@gmail.com>
2024-12-12 09:55:42 +0530

    second commit

--- face-recognition-app/src/components/Home.js
+++ face-recognition-app/src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <h1>Welcome to the Face Recognition App</h1>
       <Link to="/add-person" className="button">Add a New Person</Link>
       <br />
-      <Link to="../../face_recognition_app.py" className="button">Start Face Recognition</Link>
+      <Link to="face_recognition_app" className="button">Start Face Recognition</Link>
     </div>
   );
 }
